f6c98333ade61e1d4d0bce5abaca2bece972464b
Xing Chen <u7725171@anu.edu.au>
2024-05-28 23:26:05 +1000

    Initial commit

---
+++ build/CMakeFiles/snowSimulation.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/air_star/Documents/ANU/2024Sem1/Computer Graphics/Group Project/AccumulatedSnow/SnowSimulation/source/SnowCloud.cpp" "CMakeFiles/snowSimulation.dir/source/SnowCloud.cpp.o" "gcc" "CMakeFiles/snowSimulation.dir/source/SnowCloud.cpp.o.d"
+  "/Users/air_star/Documents/ANU/2024Sem1/Computer Graphics/Group Project/AccumulatedSnow/SnowSimulation/source/SnowFall.cpp" "CMakeFiles/snowSimulation.dir/source/SnowFall.cpp.o" "gcc" "CMakeFiles/snowSimulation.dir/source/SnowFall.cpp.o.d"
+  "/Users/air_star/Documents/ANU/2024Sem1/Computer Graphics/Group Project/AccumulatedSnow/SnowSimulation/source/SnowFlake.cpp" "CMakeFiles/snowSimulation.dir/source/SnowFlake.cpp.o" "gcc" "CMakeFiles/snowSimulation.dir/source/SnowFlake.cpp.o.d"
+  "/Users/air_star/Documents/ANU/2024Sem1/Computer Graphics/Group Project/AccumulatedSnow/SnowSimulation/source/SnowOverlay.cpp" "CMakeFiles/snowSimulation.dir/source/SnowOverlay.cpp.o" "gcc" "CMakeFiles/snowSimulation.dir/source/SnowOverlay.cpp.o.d"
+  "/Users/air_star/Documents/ANU/2024Sem1/Computer Graphics/Group Project/AccumulatedSnow/SnowSimulation/source/SnowScene.cpp" "CMakeFiles/snowSimulation.dir/source/SnowScene.cpp.o" "gcc" "CMakeFiles/snowSimulation.dir/source/SnowScene.cpp.o.d"
+  "/Users/air_star/Documents/ANU/2024Sem1/Computer Graphics/Group Project/AccumulatedSnow/SnowSimulation/source/Surface.cpp" "CMakeFiles/snowSimulation.dir/source/Surface.cpp.o" "gcc" "CMakeFiles/snowSimulation.dir/source/Surface.cpp.o.d"
+  "/Users/air_star/Documents/ANU/2024Sem1/Computer Graphics/Group Project/AccumulatedSnow/SnowSimulation/source/main.cpp" "CMakeFiles/snowSimulation.dir/source/main.cpp.o" "gcc" "CMakeFiles/snowSimulation.dir/source/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
